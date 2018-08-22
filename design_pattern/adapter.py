66b81920dce66f9bad8af0287edcc215dba81173
yangwenxing <ningsi_yangup@163.com>
2018-08-22 22:23:51 +0800

    适配器模式

--- design_pattern/adapter.py
+++ design_pattern/adapter.py
@@ -9,3 +9,44 @@ class MobileUnlock:
     def unlock(self):
         pass
 
+
+# 密码解锁
+class PwdUnlock(MobileUnlock):
+
+    def unlock(self):
+        print("密码解锁")
+
+
+# 指纹解锁
+class FingerprintUnlock(MobileUnlock):
+
+    def unlock(self):
+        print("指纹解锁")
+
+
+# 人脸解锁 (待适配类)
+class FaceUnlock(MobileUnlock):
+
+    def face_unlock(self):
+        print("人脸解锁")
+
+
+# 适配类
+class Adapter(MobileUnlock):
+
+    def __init__(self):
+        self.face = FaceUnlock()
+
+    def unlock(self):
+        self.face.face_unlock()
+
+
+if __name__ == '__main__':
+
+    password = PwdUnlock()
+    finger = FingerprintUnlock()
+    face = Adapter()
+
+    password.unlock()
+    finger.unlock()
+    face.unlock()
