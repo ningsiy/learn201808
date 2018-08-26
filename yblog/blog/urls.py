b1d8367a2f4a9aa7f9a901e0a5e643963de7b066
yangwenxing <ningsi_yangup@163.com>
2018-08-26 19:46:35 +0800

    Django

--- yblog/blog/urls.py
+++ yblog/blog/urls.py
@@ -3,8 +3,11 @@ from . import views
 
 
 urlpatterns = [
-    path('index/', views.index),
+    path('index/', views.index, name='index'),
 
     # Django 2.0 版本新特性  路由参数的变化（以前是(?P<article_id>[0-9]{4})）
     path('article/<int:article_id>', views.article_page, name='article_page'),
+    path('edit/<int:article_id>', views.edit_page, name='edit_page'),
+    path('edit_action', views.edit_action, name='edit_action'),
+    path('delete/<int:article_id>', views.delete_article, name='delete_article'),
 ]
